fe63404c5cd512b810b376502761ae99d9ab566d
dok <jorgecbll@gmail.com>
2016-05-30 17:35:30 -0400

    Its a template now... i dont know how to change it dinamically.

--- js/main.js
+++ js/main.js
@@ -1,50 +1,55 @@
+Vue.config.debug = true;
+Vue.component('channels', {
+    template: '#get-channel',
 
-const vm = new Vue({
-  el: '#app',
-
-  ready: function() {
+    data: function(){
+      return {
+        streamProfile: [],
+        channelProfile: [],
+        channel: 'TSM_Dyrus'
+      }
+    },
+    ready: function(){
       this.getStream();
     },
-  data: {
-    streamProfile: [],
-    channelProfile: [],
-    channel: 'Aphromoo'
-  },
-  methods: {
-    getStream: function (){
-      this.$http.get('https://api.twitch.tv/kraken/streams/'+this.channel, function(data) {
-        this.$set('streamProfile', data)
-      }).catch(function(data, status, request){
-        console.log(status, request);
-      }).then(function(){
-        if(this.streamProfile.stream === null){
-          this.getChannel();
-        }
-      })
-
-    },
-    getChannel: function() {
-      this.$http.get(this.streamProfile._links.channel, function(data) {
-        this.$set('channelProfile', data)
-      }).catch(function(data, status, request){
-        console.log('error', status, request)
-      })
-    }
-  }
-
-})
-
-/*
-
-this.$http.get('http://httpbin.org/ip', function (data) {
-            // set data on vm
-            this.$set('origin', data)
-
-        }).error(function (data, status, request) {
-            // handle error
+    methods: {
+      getStream: function (){
+        this.$http.get('https://api.twitch.tv/kraken/streams/'+this.channel, function(data) {
+          this.$set('streamProfile', data)
+        }).catch(function(data, status, request){
+          console.log(status, request);
+        }).then(function(){
+          if(this.streamProfile.stream === null){
+            this.getChannel();
+          }
         })
 
+      },
+      getChannel: function() {
+        this.$http.get(this.streamProfile._links.channel, function(data) {
+          this.$set('channelProfile', data)
+        }).catch(function(data, status, request){
+          console.log('error', status, request)
+        })
       }
+    },
+    props: ['stream']
 
-https://www.twitch.tv/aphromoo
-*/
+});
+
+
+new Vue({
+
+  el: '#app',
+
+  components: 'channels',
+
+  data: {
+    channels: [
+      'freecodecamp',
+      'hastad'
+    ]
+  }
+
+
+})
