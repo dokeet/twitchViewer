c6812472e3ca73b28b849940ba66a3cf89f8fd0a
dok <jorgecbll@gmail.com>
2016-06-02 19:47:41 -0400

    Separated in two components, online and offline, i need to find a better way to do that

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 Vue.config.debug = true;
-Vue.component('channels', {
-    template: '#get-channel',
+Vue.component('channels-online', {
+    template: '#channels-online',
 
     props: ['channels'],
 
@@ -8,7 +8,8 @@ Vue.component('channels', {
       return {
         streamProfile: [],
         channelProfile: [],
-        channel: this.channels.channel
+        channel: this.channels.channel,
+        status: true
       }
     },
     ready: function(){
@@ -22,6 +23,7 @@ Vue.component('channels', {
           console.log(status, request);
         }).then(function(){
           if(this.streamProfile.stream === null){
+            this.status = false
             this.getChannel();
           }
         })
@@ -33,7 +35,51 @@ Vue.component('channels', {
         }).catch(function(data, status, request){
           console.log('error', status, request)
         })
+      },
+
+    },
+
+
+});
+
+Vue.component('channels-offline', {
+    template: '#channels-offline',
+
+    props: ['channels'],
+
+    data: function(){
+      return {
+        streamProfile: [],
+        channelProfile: [],
+        channel: this.channels.channel,
+        status: true
       }
+    },
+    ready: function(){
+      this.getStream(this.channel);
+    },
+    methods: {
+      getStream: function (channel){
+        this.$http.get('https://api.twitch.tv/kraken/streams/'+channel, function(data) {
+          this.$set('streamProfile', data)
+        }).catch(function(data, status, request){
+          console.log(status, request);
+        }).then(function(){
+          if(this.streamProfile.stream === null){
+            this.status = false
+            this.getChannel();
+          }
+        })
+
+      },
+      getChannel: function() {
+        this.$http.get(this.streamProfile._links.channel, function(data) {
+          this.$set('channelProfile', data)
+        }).catch(function(data, status, request){
+          console.log('error', status, request)
+        })
+      },
+
     },
 
 
@@ -50,11 +96,13 @@ new Vue({
       {channel: 'hastad'},
       {channel: 'TSM_Dyrus'},
       {channel: 'Keireth'},
-      {channel: 'TSM_Bjergsen'}
+      {channel: 'TSM_Bjergsen'},
+      {channel: 'imaqtpie'},
+      {channel: 'Trick2g'},
+      {channel: 'comster404'}
     ],
-    channelProfile: {},
-    streamProfile: {}
-  }
+
+  },
 
 
 })
