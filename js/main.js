f1e9ba6748159519635824d26210b8abf16da123
dok <jorgecbll@gmail.com>
2016-06-07 03:39:28 -0400

    Now im using one component with the same method, DRY.   TODO: Order the components by status

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 Vue.config.debug = true;
-Vue.component('channels-online', {
-    template: '#channels-online',
+
+Vue.component('channels', {
+    template: '#channels',
 
     props: ['channels'],
 
@@ -9,7 +10,7 @@ Vue.component('channels-online', {
         streamProfile: [],
         channelProfile: [],
         channel: this.channels.channel,
-
+        channelStatus: true
       }
     },
     ready: function(){
@@ -17,16 +18,18 @@ Vue.component('channels-online', {
     },
     methods: {
       getStream: function (channel){
-        this.$http.get('https://api.twitch.tv/kraken/streams/'+channel, function(data) {
+        this.$http.get('https://api.twitch.tv/kraken/streams/'+channel, function(data, status, request) {
           this.$set('streamProfile', data)
-        }).catch(function(data, status, request){
-            this.$log(status, request)
-
-        }).then(function(){
+          }).then(function(){
           if(this.streamProfile.stream === null){
-
+            this.channelStatus = false
             this.getChannel();
           }
+
+        }).catch(function(data, status, request){
+            this.channelStatus = false
+            this.$set('channelProfile', data)
+            this.$log(status, request, data)
         })
 
       },
@@ -40,65 +43,22 @@ Vue.component('channels-online', {
 
     },
 });
-
-Vue.component('channels-offline', {
-    template: '#channels-offline',
-
-    props: ['channels'],
-
-    data: function(){
-      return {
-        streamProfile: [],
-        channelProfile: [],
-        channel: this.channels.channel,
-        wrongChannels: [],
-      }
-    },
-    created: function(){
-      this.getStream(this.channel);
-    },
-    methods: {
-      getStream: function (channel){
-        this.$http.get('https://api.twitch.tv/kraken/streams/'+channel, function(data) {
-          this.$set('streamProfile', data)
-
-        }).then(function(){
-            if(this.streamProfile.stream === null){
-
-              this.getChannel();
-            }
-          }).catch(function(data, status, request){
-            this.$log(status)
-
-            console.log('error ok')
-          });
-      },
-      getChannel: function() {
-        this.$http.get(this.streamProfile._links.channel, function(data) {
-          this.$set('channelProfile', data)
-        }).catch(function(data, status, request){
-
-        })
-      },
-    },
-});
-
-
 new Vue({
 
   el: '#app',
 
   data: {
+
     channels: [
-      {channel: 'freecodecamp'},
-      {channel: 'hastad'},
-      {channel: 'TSM_Dyrus'},
-      {channel: 'Keireth'},
-      {channel: 'TSM_Bjergsen'},
-      {channel: 'imaqtpie'},
-      {channel: 'Trick2g'},
-      {channel: 'flosd'},
-      {channel: 'brtt'}
+      {channel: 'freecodecamp', status: null},
+      {channel: 'hastad', status: null},
+      {channel: 'TSM_Dyrus', status: null},
+      {channel: 'Keireth', status: null},
+      {channel: 'TSM_Bjergsen', status: null},
+      {channel: 'imaqtpie', status: null},
+      {channel: 'Trick2g', status: null},
+      {channel: 'flosd', status: null},
+      {channel: 'camfoster404', status: null}
     ],
 
   },
